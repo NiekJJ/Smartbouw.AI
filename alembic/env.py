96df8894e162a7d83d17be1846c2de9e070cf0d6
NiekJJ <nieksoftwareai@outlook.com>
2025-06-10 15:40:50 +0200

    Fix Alembic import path

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from database import Base  # database.py zit in de root van je project
+from app.database import Base
 
 target_metadata = Base.metadata
 
